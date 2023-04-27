e4c157a08f44897e25e6697ea6736ad8c9188ea5
Brean0 <midoryia33@proton.me>
2023-04-27 01:43:11 -0500

    update

--- test/foundry/UnbanksyPOC.t.sol
+++ test/foundry/UnbanksyPOC.t.sol
@@ -20,6 +20,14 @@ contract UnbanksyPOC is TestHelper {
     address OHM = 0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5;
     address OHMFRAXBP = 0x5271045F7B73c17825A7A7aee6917eE46b0B7520;
     address yOHMFRAXBP = 0x7788A5492bc948e1d8c2caa53b2e0a60ed5403b0;
+    
+    address USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address FRAXBPPOOL = 0xDcEF968d416a41Cdac0ED8702fAC8128A64241A2;
+    address FRAXROUTER = 0x25e9acA5951262241290841b6f863d59D37DC4f0;
+    address CVXFPIS = 0xfA87DB3EAa93B7293021e38416650D2E666bC483;
+    address FPIS = 0xc2544A32872A91F4A553b404C6950e89De901fdb;
+
+
 
     function setUp() public {
         deployMockTokens(5);
@@ -29,16 +37,15 @@ contract UnbanksyPOC is TestHelper {
         weth = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     }
 
-
-    function testUnBanksy() prank(user) public {
+    function testUnBanksyPipeline() prank(user) public {
         // POC tx that: 
         // 1: unstakes gOHM -> OHM
         // 2: LPs OHM into OHM-FRAXBP pool 
-        // 3: deposits OHM-FRAXBP convex vault
+        // 3: deposits OHM-FRAXBP yearn vault
         vm.pauseGasMetering(); // disable gas metering for calculating gas costs
 
         // deal 100 gOHM to the user:
-        deal(address(gOHM), user, 100e18);
+        deal(address(gOHM), user, 1e18);
         
         AdvancedPipeCall[] memory _advancedPipeCall = new AdvancedPipeCall[](4);
 
@@ -70,7 +77,7 @@ contract UnbanksyPOC is TestHelper {
         // we want to get the amount of OHM-FRAXBP we got from the last call
         data = abi.encodeWithSignature(
             "deposit(address,address,uint256)",
-            address(0x7788A5492bc948e1d8c2caa53b2e0a60ed5403b0), //OHMFRAXBP yearn vault
+            address(0x7788A5492bc948e1d8c2caa53b2e0a60ed5403b0), // OHMFRAXBP yearn vault
             address(0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52), // parther vault ??
             0 // this is the amount, but since we're getting it from the 2nd call, it is set to 0 (as its being overriden anyways)
         );
@@ -113,7 +120,7 @@ contract UnbanksyPOC is TestHelper {
                 depot.transferToken.selector,
                 IERC20(gOHM),
                 address(PIPELINE),
-                100e18,
+                1e18,
                 From.EXTERNAL,
                 To.EXTERNAL
             );
@@ -154,6 +161,69 @@ contract UnbanksyPOC is TestHelper {
 
         // convert farmcalls into bytes
         depot.farm(_farmCalls);
+
+        vm.pauseGasMetering(); // resume gas costs
+        console.log("user's balance of gOHM after: ", IERC20(gOHM).balanceOf(user));
+        console.log("user's balance of yOHMFRAX-BP after: ", IERC20(yOHMFRAXBP).balanceOf(user));
+        vm.resumeGasMetering();
+    }
+
+    function testUnBanksyNoPipeline() prank(user) public {
+        // POC tx that: 
+        // 1: unstakes gOHM -> OHM
+        // 2: LPs OHM into OHM-FRAXBP pool 
+        // 3: deposits OHM-FRAXBP yearn vault
+        vm.pauseGasMetering(); // disable gas metering for calculating gas costs
+
+        // deal 100 gOHM to the user:
+        deal(address(gOHM), user, 1e18);
+        console.log("user's balance of gOHM before: ", IERC20(gOHM).balanceOf(user));
+        console.log("user's balance of yOHMFRAX-BP before: ", IERC20(yOHMFRAXBP).balanceOf(user));
+
+        vm.resumeGasMetering(); // resume gas costs
+        // approve gOHM for unstaking
+        IERC20(gOHM).approve(OLYMPUS_STAKING, 1000e18); // 1
+        // unstake gOHM -> OHM
+        OLYMPUS_STAKING.call(
+            abi.encodeWithSignature(
+                "unstake(address,uint256,bool,bool)",
+                address(user),
+                1e18,
+                false,
+                false
+            )
+        ); // 2
+
+        // approve OHM for curve
+
+        IERC20(OHM).approve(
+            address(0xFc1e8bf3E81383Ef07Be24c3FD146745719DE48D), 
+            1000e18
+        ); // 3
+        // LP OHM into OHM-FRAXBP pool
+
+        address(0xFc1e8bf3E81383Ef07Be24c3FD146745719DE48D).call(
+            abi.encodeWithSignature(
+                "add_liquidity(uint256[2],uint256,bool)",
+                [ uint256(264976083918), 0],
+                uint256(0),
+                false
+            )
+        ); // 4 
+
+        IERC20(OHMFRAXBP).approve(
+            address(0x8ee392a4787397126C163Cb9844d7c447da419D8), 
+            1000e18
+        ); // 5
+
+        address(0x8ee392a4787397126C163Cb9844d7c447da419D8).call(
+            abi.encodeWithSignature(
+                "deposit(address,address,uint256)",
+                address(0x7788A5492bc948e1d8c2caa53b2e0a60ed5403b0), // OHMFRAXBP yearn vault
+                address(0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52), // parther vault ??
+                427118414574078207231
+            )
+        ); // 6 
 
         vm.pauseGasMetering(); // resume gas costs
         console.log("user's balance of gOHM after: ", IERC20(gOHM).balanceOf(user));
